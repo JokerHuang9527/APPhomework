a2f2678afa2da68435199d33e7c87f5430f12ea4
110360131”git config --global user.name 110360131” <user@gmail.comgit config --global user.email huangpw1031@gmail.com>
2023-10-10 23:32:30 +0800

    master

---
+++ src/homework2/homework2.java
@@ -0,0 +1,11 @@
+package homework2;
+
+public class homework2
+{
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("歡迎使用JAVA!!!!");
+		System.out.print("開始使用JAVA吧!!");
+	}
+}
